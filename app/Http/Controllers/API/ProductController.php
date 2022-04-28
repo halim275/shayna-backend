0235008ecfca5b6c986e931e92c705d4a1a7a711
halimyusuf027 <muhammadhalimyusuf@gmail.com>
2022-04-28 13:45:57 +0700

    API Product

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -25,5 +25,34 @@ class ProductController extends Controller
 			else
 				return ResponseFormatter::error(null, 'Data Produk tidak ada', 404);
 		}
+
+		if ($slug) {
+			$product = Product::with('galleries')
+				->where('slug', $slug)
+				->first();
+			if ($product)
+				return ResponseFormatter::success($product, 'Data Produk berhasil diambil');
+			else
+				return ResponseFormatter::error(null, 'Data Produk tidak ada', 404);
+		}
+
+		$product = Product::with('galleries');
+
+		if ($name)
+			$product->where('name', 'like', '%' . $name . '%');
+
+		if ($type)
+			$product->where('type', 'like', '%' . $type . '%');
+
+		if ($price_from)
+			$product->where('price', '>=', $price_from);
+
+		if ($price_to)
+			$product->where('price', '<=', $price_to);
+
+		return ResponseFormatter::success(
+			$product->paginate($limit),
+			'Data list produk berhasil diambil'
+		);
 	}
 }
