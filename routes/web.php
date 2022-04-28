c52968780512b378c6660ac4c5c4adbbb1fb6eae
halimyusuf027 <muhammadhalimyusuf@gmail.com>
2022-04-28 10:10:37 +0700

    new web

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,4 @@ Route::resource('products', 'ProductController');
 Route::resource('product-galleries', 'ProductGalleryController');
 
 Route::get('transactions/{id}/set-status', 'TransactionController@setStatus')->name('transactions.status');
-
 Route::resource('transactions', 'TransactionController');
